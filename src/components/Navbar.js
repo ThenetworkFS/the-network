c65197496f7702aa0ba677fc75fadc1d1d224b3f
Gabriel Messager <gabriel@frame.io>
2018-03-19 11:38:03 -0400

    remove commented out code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,37 +4,6 @@ import { removeUser } from '../store'
 import { fire, db } from '../fire'
 import history from '../history'
 import { connect } from 'react-redux'
-
-// class Navbar extends React.Component {
-//   constructor(props) {
-//     super(props)
-//   }
-  
-//   render() {
-//     const user= this.props.loggedInUser
-//     return (
-//       <div>
-//         <h1>The Network</h1>
-//         <nav>
-//         { user.email ?
-//           <div>
-//           <Link to="/home">Home</Link>
-//           <Link to="/profile">Profile</Link>
-//           <a href="#" onClick={this.props.removeUser}>
-//             Sign out
-//           </a>
-//           </div>
-//           :
-//           <div>
-//           <Link to="/">Login</Link>
-//           <Link to="/signup">Signup</Link>
-//           </div>
-//           }
-//         </nav>
-//       </div>
-//     )
-//   }
-// }
 import { Icon, Menu } from 'semantic-ui-react'
 
 class Navbar extends Component {
