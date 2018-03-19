d8052d1d2d9c79b698d7512d8d69d31fabef401d
Beth Barnes <bethcbarnes@outlook.com>
2018-03-19 16:17:43 -0400

    feat(AllUsers): add users to state, render on page

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { fire, db } from '../fire'
+import { connect } from 'react-redux'
+import history from '../history'
 
-export default class AllUsers extends Component {
+class AllUsers extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state={
+      users:[]
+    }
+  }
+
+  componentDidMount(){
+    let currentComponent = this
+
+    db.collection("users")
+    .onSnapshot(function(querySnapshot) {
+      querySnapshot.docChanges.forEach((change) => {
+        if(change.type === "added"){
+          currentComponent.setState({
+            users: currentComponent.state.users.concat(change.doc.data())
+          });
+        }
+    });
+  })
+    }
 
   render() {
-
-    const data = db.collection("users").get();
-    console.log('DATA ', data)
-
-    db.collection("users").get().then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        console.log(doc.data())
-      });
-    })
-
     return (
       <div>
         <h1>All Users </h1>
+        {this.state.users.map((user) => {
+      return (
+        <div>
+          <h1> {user.firstName} {user.lastName} </h1>
+          <br></br>
+        </div>
+      )
+    })}
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => ({ loggedInUser: state.user.loggedInUser })
+
+export default connect(mapStateToProps)(AllUsers)
+
+
