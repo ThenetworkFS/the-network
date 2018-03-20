b2636777a5d4f03a45146aa538c9ce9245cc7a74
Beth Barnes <bethcbarnes@outlook.com>
2018-03-20 11:53:24 -0400

    feat(search): able to search by Name

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -5,109 +5,75 @@ import { connect } from 'react-redux'
 import history from '../history'
 import { Search, Grid, Header } from 'semantic-ui-react'
 import _ from 'lodash'
-
-
-// const source = this.state.users;
-
-// [{"title": "Beth", description: 'A cool person', price: 999999 },{"title": "Johnny", description: 'Another coolio', price: 2.50 } ]
-
+import { Input } from 'semantic-ui-react'
 
 class AllUsers extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state={
-      users:[],
-      results: [],
-      isLoading: false,
-      value: ''
+    this.state = {
+      users: [],
+      searchVal: ''
     }
-    this.resetComponent = this.resetComponent.bind(this)
-    this.handleResultSelect = this.handleResultSelect.bind(this)
-    this.handleSearchChange = this.handleSearchChange.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
   componentWillMount() {
-    this.resetComponent()
   }
 
-  componentDidMount(){
+  componentDidMount() {
     let currentComponent = this
     db.collection("users")
-    .onSnapshot(function(querySnapshot) {
-      querySnapshot.docChanges.forEach((change) => {
-        if(change.type === "added"){
-          currentComponent.setState({
-            users: currentComponent.state.users.concat(change.doc.data())
-          });
-        }
-    });
-  })
-    }
+      .onSnapshot(function (querySnapshot) {
+        querySnapshot.docChanges.forEach((change) => {
+          if (change.type === "added") {
+            currentComponent.setState({
+              users: currentComponent.state.users.concat(change.doc.data())
+            });
+          }
+        });
+      })
+  }
 
-    resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
-
-      handleResultSelect = (e, { result }) => this.setState({ value: result.title })
-
-      handleSearchChange = (e, { value }) => {
-        this.setState({ isLoading: true, value })
-        // async call / dispatch / similar
-
-        // ({ image, price, title, description }) => [
-        //   image && <div key='image' className='image'>{createHTMLImage(image)}</div>,
-        //   <div key='content' className='content'>
-        //     {price && <div className='price'>{price}</div>}
-        //     {title && <div className='title'>{title}</div>}
-        //     {description && <div className='description'>{description}</div>}
-        //   </div>,
-        // ]
-
-          if (value.length < 1) return this.resetComponent()
-
-          const re = new RegExp(_.escapeRegExp(value), 'i')
-
-          const isMatch = result => re.test(result.title)
-          // console.log('did we match? ', isMatch)
-
-          this.setState({
-            isLoading: false,
-            results: _.filter(this.state.users, isMatch),
-          })
-          // console.log('re: ', re, 'results: ' , this.state.results)
-      }
+  handleInputChange(event){
+    event.preventDefault()
+    this.setState({
+      searchVal: event.target.value
+    })
+  }
 
 
 
   render() {
-    const { isLoading, value, results } = this.state
 
     console.log('STATE: ', this.state)
-    return (
-      <Grid>
-        <Grid.Column width={8}>
-          <Search
-            loading={isLoading}
-            onResultSelect={this.handleResultSelect}
-            onSearchChange={this.handleSearchChange}
-            results={[{ firstName: '', lastName: '' }]}
-            resultRenderer={({ firstName, lastName}) => [
-              <div key='content' className='content'>
-                {firstName && <div className='firstName'>{firstName}</div>}
-                {lastName && <div className='lastName'>{lastName}</div>}
 
-              </div>,
-            ]}
-            value={value}
-            {...this.props}
-          />
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Header>State</Header>
-          <pre>{JSON.stringify(this.state, null, 2)}</pre>
-          <Header>Options</Header>
-          <pre>{JSON.stringify(this.state.users, null, 2)}</pre>
-        </Grid.Column>
-      </Grid>
+    const filteredUsers = this.state.users.filter((user) => {
+      return user.firstName.includes(this.state.searchVal)
+    })
+
+    return (
+      <div>
+        <Input
+          onChange={this.handleInputChange}
+          icon={{ name: 'search', circular: true, link: true }}
+          placeholder='Search...'
+        />
+        {this.state.searchVal
+          ? filteredUsers.map((user => {
+            return (
+              <h1>{user.firstName}</h1>
+              )
+            })
+          )
+          : this.state.users.map((user => {
+            return (
+              <h1>{user.firstName}</h1>
+              )
+            })
+          )
+        }
+      </div>
     )
   }
 }
