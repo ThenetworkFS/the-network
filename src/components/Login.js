f395d4c400b5a6c306bb12bef70a99537291c862
Gabriel Messager <gabriel@frame.io>
2018-03-21 15:13:45 -0400

    feat(client): style login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,16 @@ import { startFetch } from '../store'
 import { connect } from 'react-redux'
 import history from '../history'
 import firebase from 'firebase'
-import { Button, Icon, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { 
+  Button,
+  Icon,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from 'semantic-ui-react'
 
 
 class Login extends React.Component {
@@ -43,9 +52,9 @@ class Login extends React.Component {
               verticalAlign="middle"
             >
               <Grid.Column style={{ maxWidth: 450 }}>
-                <Header as="h2" color="teal" textAlign="center">
+                <Header as="h2" color="black" textAlign="center">
                   <Image src='https://yt3.ggpht.com/a-/AJLlDp1nApHN8lojbFAL50lLxFOgBqCqtvcO1Ebl0Q=s900-mo-c-c0xffffffff-rj-k-no' />
-                  {' '}Log-in to your account
+                  {' '}The Network
                 </Header>
                 <Form onSubmit={this.signInAnonymously} size="large">
                   <Segment>
@@ -64,16 +73,16 @@ class Login extends React.Component {
                       placeholder="Password"
                       type="password"
                     />
-                    <Button color="teal" fluid size="large" type="submit">Login</Button>
+                    <Button className="login-form-button" color="teal" fluid size="large" type="submit">Login</Button>
                   </Segment>
                 </Form>
-                <Message>
-                  New to us? <a href="/signup">Sign Up</a>
-                </Message>
+                <h5 className="login-form-prompt">
+                  Don't have an account? <a href="/signup">Sign Up Now</a>
+                </h5>
                 <h4>OR</h4>
-                <Button color="teal" size="small" type="submit">
-                  {`Login with `}
-                  <Icon name="google" className="large google icon" onClick={this.googleLogin} />
+                <Button className="google-login-button"color="blue" size="small" type="submit">
+                  <span>Login with</span>
+                  <Icon name="google" className="large google icon google-login-icon" onClick={this.googleLogin} />
                 </Button>
               </Grid.Column>
             </Grid>
