c8abaa66fb8ef96f7c8b4a0c997f68485ea3fd00
Tara Whirley <whirleyt@outlook.com>
2018-03-21 17:32:47 -0400

    fix(navbar): routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component {
             <div>
               <Menu icon>
                 <Menu.Item name='The Network' />
-                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
+                <Menu.Item name='home/news' active={activeItem === 'home/news'} onClick={this.handleItemClick}>
                   <Icon name='browser' />
                 </Menu.Item>
 
