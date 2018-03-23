16e2e1f54b20fa322b5e4ff37d8ae2ad8cb88f3b
Gabriel Messager <gabriel@frame.io>
2018-03-23 11:15:44 -0400

    fix(client): style userProfile and EditProfile

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -6,13 +6,15 @@ import history from '../history'
 import { getUser } from '../store' 
 import EditWork from './EditWork'
 import EditProject from './EditProject'
-
+import { Card, Form, Input, Button } from 'semantic-ui-react'
+import { ANONYMOUS_USER_IMAGE_URL } from '../constants'
 
 class EditProfile extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       loggedInUser: props.loggedInUser,
+      isProfileSaved: false,
     }
   }
 
@@ -42,12 +44,12 @@ class EditProfile extends React.Component {
 
   handleChange = event => {
     event.preventDefault()
-    console.log(event.target.name, event.target.value)
     this.setState({
       loggedInUser: {
         ...this.state.loggedInUser,
         [event.target.name]: event.target.value,
       },
+      isProfileSaved: true,
     })
   }
 
@@ -58,79 +60,101 @@ class EditProfile extends React.Component {
   render() {
     const user = this.state.loggedInUser
     return (
-      <div>
-        <form
-          onSubmit={this.handleProfileSubmit}
-          onChange={this.handleChange}
-        >
-          <h6>
-            <input
-              onChange={this.noop}
-              name="firstName"
-              value={user.firstName}
-            />{' '}
-            <input
-              onChange={this.noop}
-              name="lastName"
-              value={user.lastName}
-            />
-          </h6>
+      <div className="user-profile-container">
+        <Card className="user-profile">
+          <div></div>
+          <img
+            alt="user"
+            src={user.image ? user.image : ANONYMOUS_USER_IMAGE_URL}
+            className={!user.image ? "user-profile-image anonymous" : "user-profile-image"}
+          />
           <ImagePicker />
-          <h6>
-            Lives in{' '}
-            <input
-              onChange={this.noop}
-              name="city"
-              placeholder="City"
-              value={user.city}
-            />,{' '}
-            <input
-              onChange={this.noop}
-              name="state"
-              placeholder="State"
-              value={user.state}
-            />{' '}
-            <input
-              onChange={this.noop}
-              name="country"
-              placeholder="Country"
-              value={user.country}
-            />
-          </h6>
-            Is interested in:{' '}
-            <input
-              onChange={this.noop}
-              name="interests"
-              value={user.interests}
-            />
-          <h6>
-            Slack:{' '}
-            <input
-              onChange={this.noop}
-              name="slack"
-              value={user.slack}
-            />
-          </h6>
-          <h6>
-            Github:{' '}
-            <input
-              onChange={this.noop}
-              name="github"
-              value={user.github}
-            />
-          </h6>
-          <h6>
-            Linkedin:{' '}
-            <input
-              onChange={this.noop}
-              name="linkedin"
-              value={user.linkedin}
-            />
-          </h6>
-          <button type="submit">Save Profile</button>
-        </form>
-        <EditWork />
-        <EditProject />
+          <Form
+            onSubmit={this.handleProfileSubmit}
+            onChange={this.handleChange}
+          >
+            <div className="user-profile-username edit-profile">
+              <Form.Field>
+                <label className="label">First name</label>
+                <Input
+                  type='text'
+                  onChange={this.noop}
+                  name="firstName"
+                  value={user.firstName}
+                />
+                <label className="label">Last name</label>
+                <Input
+                  type="text"
+                  onChange={this.noop}
+                  name="lastName"
+                  value={user.lastName}
+                />
+              </Form.Field>
+            </div>
+            <div>
+              <Form.Field>
+                <label className="label">City</label>
+                <Input
+                  type='text'
+                  onChange={this.noop}
+                  name="city"
+                  value={user.city}
+                />
+                <label className="label">State</label>
+                <Input
+                  type="text"
+                  onChange={this.noop}
+                  name="state"
+                  value={user.state}
+                />
+                <label className="label">Country</label>
+                <Input
+                  type="text"
+                  onChange={this.noop}
+                  name="country"
+                  value={user.country}
+                />
+                <label className="label">Your interests</label>
+                <Input
+                  type="text"
+                  onChange={this.noop}
+                  name="interests"
+                  value={user.interests}
+                />
+                <label className="label">Slack</label>
+                <Input
+                  type="text"
+                  onChange={this.noop}
+                  name="slack"
+                  value={user.slack}
+                />
+                <label className="label">Github</label>
+                <Input
+                  type="text"
+                  onChange={this.noop}
+                  name="github"
+                  value={user.github}
+                />
+                <label className="label">LinkedIn</label>
+                <Input
+                  type="text"
+                  onChange={this.noop}
+                  name="linkedin"
+                  value={user.linkedin}
+                />
+              </Form.Field>
+            </div>
+            <Button 
+              disabled={this.state.isProfileSaved}
+              className="user-profile-save-button"
+              color="blue"
+            >
+              Save Profile
+            </Button>
+          </Form>
+          <EditWork />
+          <EditProject />
+        </Card>
       </div>
     )
   }
