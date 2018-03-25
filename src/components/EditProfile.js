49568de3eaae8e7d9f8a8463e9672ad31710f14c
Gabriel Messager <gabriel@frame.io>
2018-03-25 17:52:20 -0400

    fix(client): fix form to edit user

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -17,6 +17,7 @@ class EditProfile extends React.Component {
       loggedInUser: props.loggedInUser,
       isProfileSaved: false,
       address: '',
+      isAdressedEdited: false,
     }
   }
 
@@ -42,7 +43,7 @@ class EditProfile extends React.Component {
   handleProfileSubmit = (event) => {
     event.preventDefault()
     this.setState({ isProfileSaved: true })
-    if (this.state.address.length) {
+    if (this.state.isAdressedEdited) {
       this.getGeoCodeByAddress()
       .then(result => {
         const userWorkInfo = {
@@ -54,6 +55,7 @@ class EditProfile extends React.Component {
         this.updateUser(userWorkInfo)
       })
     } else {
+      console.log('STATE', this.state.loggedInUser)
       this.updateUser(this.state.loggedInUser)
     }
   }
@@ -87,6 +89,7 @@ class EditProfile extends React.Component {
 
   onWorkAddressChange = (address) => {
     this.setState({
+      isAdressedEdited: true,
       address,
       loggedInUser: {
         ...this.state.loggedInUser,
