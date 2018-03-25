2eb516bd72c13d193d98a9e8c69192fe591154f8
Tara Whirley <whirleyt@outlook.com>
2018-03-25 17:23:26 -0400

    feat(Calendar): add/edit/delete events

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import React from 'react'
 import { connect } from 'react-redux'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+const uuidv1 = require('uuid/v1')
 BigCalendar.momentLocalizer(moment);
 
 
@@ -34,25 +35,65 @@ class Calendar extends React.Component {
   }
 
 
-  handleSubmit = (event) => {
+  handleAddSubmit = (event) => {
     event.preventDefault()
-    this.setState({ isClicked: false })
-    const title = `${event.target.title.value} ${event.target.time.value}`
+    this.setState({ addIsClicked: false })
+    const title = `${event.target.title.value} | ${event.target.time.value}`
     const startArray = event.target.start.value.split('/')
     const endArray = event.target.end.value.split('/')
     const start = `${startArray[2]}-${startArray[0]}-${(+startArray[1] + 1)}`
     const end = `${endArray[2]}-${endArray[0]}-${(+endArray[1] + 1)}`
+    const userId = this.props.loggedInUser.id
+    const id = uuidv1()
+    const calendarEvent = {
+      title,
+      start,
+      end,
+      userId,
+      id
+    }
+    db.collection("events").doc(id).set(calendarEvent)
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      })
+  }
+
+
+  handleEditSumbit = (event, id) => {
+    event.preventDefault()
+    this.setState({ editIsClicked: false })
+    const title = `${event.target.title.value} | ${event.target.time.value}`
+    const startArray = event.target.start.value.split('/')
+    const endArray = event.target.end.value.split('/')
+    const start = `${startArray[2]}-${startArray[0]}-${(+startArray[1] + 1)}`
+    const end = `${endArray[2]}-${endArray[0]}-${(+endArray[1] + 1)}`
+    const userId = this.props.loggedInUser.id
     const calendarEvent = {
       title,
       start,
       end
     }
-    db.collection("events").add(calendarEvent).then((doc) => {
-      db.collection("events").doc(doc.id).update({ id: doc.id })
-    })
+    db.collection("events").doc(id).update(calendarEvent)
       .catch(function (error) {
-        console.error("Error adding document: ", error);
+        console.error("Error updating document: ", error);
       })
+    const filtered = this.state.events.filter(e => e.id !== id)
+    this.setState({ events: filtered.concat({ title, start, end, id, userId }) })
+  }
+
+
+  deleteEvent = (event, id) => {
+    event.preventDefault()
+    db
+      .collection('events')
+      .doc(id)
+      .delete()
+      .then(() => {
+        this.setState({ editIsClicked: false })
+      })
+      .catch(err => console.error(err))
+    const newArray = this.state.events.filter(e => e.id !== id)
+    this.setState({ events: newArray })
   }
 
 
@@ -60,14 +101,14 @@ class Calendar extends React.Component {
     this.setState({ addIsClicked: true })
   }
 
-  addIsClicked = () => {
+
+  editIsClicked = () => {
     this.setState({ editIsClicked: true })
   }
 
 
   render() {
     const myEventsList = this.state.events
-    console.log(myEventsList)
     return (
       <div>
         <BigCalendar
@@ -75,46 +116,56 @@ class Calendar extends React.Component {
           events={myEventsList}
           style={{ height: "600px", width: "1200px" }}
         />
-        {this.state.isClicked ?
-          <form onSubmit={this.handleSubmit}>
-            <h6>Title</h6>
-            <input
-              name="title"
-            />
-            <h6>Time</h6>
-            <input
-              name="time"
-            />
-            <h6>State Date</h6>
-            <input
-              name="start"
-              placeholder="ex. 04/22/2018"
-            />
-            <h6>End Date</h6>
-            <input
-              name="end"
-              placeholder="ex. 04/23/2018"
-            />
-            <div>
-              <button>Add</button>
-            </div>
-          </form>
-          :
-          <button type="submit" onClick={this.addIsClicked}>Add Event</button>
+        {
+          this.state.addIsClicked ?
+            <form onSubmit={this.handleAddSubmit}>
+              <h6>Title</h6>
+              <input
+                name="title"
+              />
+              <h6>Time</h6>
+              <input
+                name="time"
+              />
+              <h6>State Date</h6>
+              <input
+                name="start"
+                placeholder="ex. 04/22/2018"
+              />
+              <h6>End Date</h6>
+              <input
+                name="end"
+                placeholder="ex. 04/23/2018"
+              />
+              <div>
+                <button>Add</button>
+              </div>
+            </form>
+            :
+            <button type="submit" onClick={this.addIsClicked}>Add Event</button>
         }
         {
           this.state.editIsClicked ?
-          <div>
-          <form>
-          <input name="title" />
-          <input name="time" />
-          <input name="startdate" />
-          <input name="enddate" />
-          </form>
-          <button>Delete</button>
-          </div>
-          : 
-          <button>Edit Your Events</button>
+            myEventsList.filter(event => event.userId === this.props.loggedInUser.id).map(calendarEvent => {
+              const startArray = calendarEvent.start.split('-')
+              const endArray = calendarEvent.end.split('-')
+              const start = `${startArray[1]}/${(+startArray[2] - 1)}/${startArray[0]}`
+              const end = `${endArray[1]}/${(+endArray[2] - 1)}/${endArray[0]}`
+              return (
+                <div key={calendarEvent.id}>
+                  <form onSubmit={(event) => this.handleEditSumbit(event, calendarEvent.id)}>
+                    <input name="title" defaultValue={calendarEvent.title.split('|')[0]} />
+                    <input name="time" defaultValue={calendarEvent.title.split('|')[1]} />
+                    <input name="start" defaultValue={start} />
+                    <input name="end" defaultValue={end} />
+                    <button type="submit">Save</button>
+                  </form>
+                  <button onClick={(event) => this.deleteEvent(event, calendarEvent.id)}>Delete</button>
+                </div>
+              )
+            })
+            :
+            <button onClick={this.editIsClicked}>Edit Your Events</button>
         }
       </div>
     )
@@ -122,7 +173,7 @@ class Calendar extends React.Component {
 }
 
 
-const mapStateToProps = ({ user: { loggedInUser }}) => ({ loggedInUser })
+const mapStateToProps = ({ user: { loggedInUser } }) => ({ loggedInUser })
 
 
 export default connect(mapStateToProps)(Calendar)
