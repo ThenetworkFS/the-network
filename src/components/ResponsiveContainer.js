1b6ade57e0c82bad878fb114fc3a86a30fb9fb49
Tara Whirley <whirleyt@outlook.com>
2018-03-26 17:43:12 -0400

    fix(HomepageLanding): refactoring

--- src/components/ResponsiveContainer.js
+++ src/components/ResponsiveContainer.js
@@ -1,36 +1,20 @@
+import React from 'react'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { startFetch } from '../store'
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility,
-} from 'semantic-ui-react'
-import { Link } from 'react-router-dom';
-import Spinner from './Spinner'
-import DesktopContainer from './DestopContainer'
+import DesktopContainer from './DesktopContainer'
 import MobileContainer from './MobileContainer'
 
 
 const ResponsiveContainer = ({ children }) => (
-    <div>
-      <DesktopContainer>{children}</DesktopContainer>
-      <MobileContainer>{children}</MobileContainer>
-    </div>
-  )
-  
-  ResponsiveContainer.propTypes = {
-    children: PropTypes.node,
-  }
-  
+  <div>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </div>
+)
+
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+
+export default ResponsiveContainer
