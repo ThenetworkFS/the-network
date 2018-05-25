89afc7c7a03dfeecb3a6b65728e3d1000aab1caf
Beth Barnes <bethcbarnes@outlook.com>
2018-05-25 17:01:40 -0400

    feat(tests): added test file for redux-1

--- src/store/fetch.test.js
+++ src/store/fetch.test.js
@@ -8,3 +8,15 @@ const shallowWithStore = (component, store) => {
 }
 
 export default shallowWithStore;
+
+
+describe('ConnectedShowBox', () => {
+  it("should render successfully if string is not provided by store", () => {
+    const testState = {
+      showBox: {}
+    };
+    const store = createMockStore(testState)
+    const component = shallowWithStore(<ConnectedShowBox />, store);
+    expect(component).to.be.a('object');
+  });
+});
